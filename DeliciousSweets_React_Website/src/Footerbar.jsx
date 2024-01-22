ed834103128f0874908469a353d27b9683082483
Victoria <piklad1996@gmail.com>
2024-01-22 16:57:18 -0600

    footer

--- DeliciousSweets_React_Website/src/Footerbar.jsx
+++ DeliciousSweets_React_Website/src/Footerbar.jsx
@@ -1,44 +1,51 @@
 import { Link } from "react-router-dom"
-
+import logo from "./logo/Delicious_Sweets.png"
 export default function Footerbar(){
     {/* Use Grid here!*/}
     return(
         <>
             <section id="footer-section">
-                <div className="footer-container">
-                    <div className="footercol">
-                        <Link to="/">Delicious Sweets logo here</Link>
-                        <p>Google Map</p>
-                        <p>Address 123 Street Ave.</p>
-                        <div>
-                            <p>[Google Map img]</p>
+                <div>
+                    <div id="columns">
+                        <div className="footercol">
+                            <img src={logo}></img> 
+                            <p>Google Map</p>
+                            <p>Address 123 Street Ave.</p>
+                            <div>
+                                <div id="google-map">
+                         
+                                </div>
+                            </div>
                         </div>
-                    </div>
 
-                    <div  className="footercol">
-                        <p>Heading One</p>
-                        <p>Address 123 Street Ave.</p>
-                        <p>Phone: 555-555-555</p>
-                        <p>Socials:</p>
-                    </div>
+                        <div  className="footercol">
+                            <p>Heading One</p>
+                            <p>Address 123 Street Ave.</p>
+                            <p>Phone: 555-555-555</p>
+                            <p>Socials:</p>
+                        </div>
 
-                    <div  className="footercol">
-                        <p>Heading Two</p>
-                        <p>Address 123 Street Ave.</p>
-                        <p>Phone: 555-555-555</p>
-                        <p>Socials:</p>
-                    </div>
+                        <div  className="footercol">
+                            <p>Heading Two</p>
+                            <p>Address 123 Street Ave.</p>
+                            <p>Phone: 555-555-555</p>
+                            <p>Socials:</p>
+                        </div>
 
-                    <div  className="footercol">
-                        <p> Heading Three</p>
-                        <input/>
-                        <button>Subscribe</button>
-                        <p>Follow Us</p>
-                        <div>
-                            <p>[Icon]</p>                         
-                            <p>[Icon]</p>    
-                            <p>[Icon]</p>                         
-                            <p>[Icon]</p>   
+                        <div  className="footercol">
+                            <p> Heading Three</p>
+                            <div>
+                                <input/>
+                                <button>Subscribe</button>
+                            </div>
+                           
+                            <p>Follow Us</p>
+                            <div className="social-btn-group">
+                                <p>[Icon]</p>                         
+                                <p>[Icon]</p>    
+                                <p>[Icon]</p>                         
+                                <p>[Icon]</p>   
+                            </div>
                         </div>
                     </div>
                 </div>
