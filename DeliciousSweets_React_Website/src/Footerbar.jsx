6c79ac217d0b877973964a77502cf5059445de96
Victoria <piklad1996@gmail.com>
2024-01-23 13:47:44 -0600

    update footer

--- DeliciousSweets_React_Website/src/Footerbar.jsx
+++ DeliciousSweets_React_Website/src/Footerbar.jsx
@@ -9,7 +9,7 @@ export default function Footerbar(){
                     <div id="columns">
                         <div className="footercol">
                             <img src={logo}></img> 
-                            <p>Google Map</p>
+                            <p className="col-heading">Google Map</p>
                             <p>Address 123 Street Ave.</p>
                             <div>
                                 <div id="google-map">
@@ -19,46 +19,49 @@ export default function Footerbar(){
                         </div>
 
                         <div  className="footercol">
-                            <p>Heading One</p>
+                            <p className="col-heading">Heading One</p>
                             <p>Address 123 Street Ave.</p>
                             <p>Phone: 555-555-555</p>
                             <p>Socials:</p>
                         </div>
 
                         <div  className="footercol">
-                            <p>Heading Two</p>
+                            <p className="col-heading" >Heading Two</p>
                             <p>Address 123 Street Ave.</p>
                             <p>Phone: 555-555-555</p>
                             <p>Socials:</p>
                         </div>
 
                         <div  className="footercol">
-                            <p> Heading Three</p>
+                            <p className="col-heading" > Heading Three</p>
                             <div>
-                                <input/>
-                                <button>Subscribe</button>
+                                <input id="subscribe-input"/>
+                                <button id="subscribe-btn">Subscribe</button>
                             </div>
                            
-                            <p>Follow Us</p>
+                        
                             <div className="social-btn-group">
-                                <p>[Icon]</p>                         
-                                <p>[Icon]</p>    
-                                <p>[Icon]</p>                         
-                                <p>[Icon]</p>   
+                            <i class="bi bi-twitter-x social-icon"></i>                        
+                            <i class="bi bi-facebook social-icon"></i>
+                            <i class="bi bi-instagram social-icon"></i>                        
+                            <i class="bi bi-youtube social-icon"></i>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                    {/* Change location?
-                    <footer>
+                <div id="bottom-text">
+                    <div>
                         <p>@CopyRight 2023</p>
-                        <div>
-                        <p>Terms of Service</p>
-                        <p>Privacy Policy</p>
+                    </div>
+
+                    <div className="info-right">
+                        <p>Terms of Service |</p>
+                        <p>Privacy Policy | </p>
                         <p>Refund Policy</p>
-                        </div>
-                    </footer> */}
+                    </div>
+                </div>
+          
             </section>
         </>
     )
