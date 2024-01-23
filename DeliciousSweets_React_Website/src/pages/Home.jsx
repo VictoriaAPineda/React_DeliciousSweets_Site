a03362e6ffc4942ea87b07f0a62be8dc4897d958
Victoria <piklad1996@gmail.com>
2024-01-23 15:54:02 -0600

    added home imgs

--- DeliciousSweets_React_Website/src/pages/Home.jsx
+++ DeliciousSweets_React_Website/src/pages/Home.jsx
@@ -1,10 +1,36 @@
+import storefront from "/src/images/storefront.jpg";
+import qualityImg from "/src/images/hands_with_flour.jpg";
+import pickupImg from "/src/images/service_img.png";
+import deliveryImg from "/src/images/delivery_img.png";
 
 export default function Home (){
 
     return(
         <>
   
-        <h1>Home Page</h1>
+            <section id="main-img-container">
+                <img src={storefront} id="storefront-img"></img>
+            </section>
+
+            <section id="info-imgs-grid-container">
+                {/* <div className="info-imgs-grid"> */}
+                    <div className="one-img">
+                        <img src={qualityImg}></img>
+                    </div>
+
+                    <div className="two-img">
+                        <img src={pickupImg}></img>
+                    </div>
+
+                    <div className="three-img"> 
+                        <img src={deliveryImg}></img>
+                    </div>
+                {/* </div> */}
+            </section>
+
+
+
+ 
         
         </>
     )
