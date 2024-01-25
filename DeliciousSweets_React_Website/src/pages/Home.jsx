8b5a9b005b30c3174994c27e4a0b6447ce53b8a9
Victoria <piklad1996@gmail.com>
2024-01-25 13:57:08 -0600

    update

--- DeliciousSweets_React_Website/src/pages/Home.jsx
+++ DeliciousSweets_React_Website/src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import storefront from "/src/images/storefront.jpg";
-import qualityImg from "/src/images/hands_with_flour.jpg";
+import qualityImg from "/src/images/siftingOverCake.png";
 import pickupImg from "/src/images/service_img.png";
 import deliveryImg from "/src/images/delivery_img.png";
+import truckIcon from "/src/images/delivery_truck.png";
 
 export default function Home (){
 
@@ -14,13 +15,16 @@ export default function Home (){
             <section id="info-imgs-container">
                 <div className="info-imgs-grid">
                     <div className="grid-item-01">
+                        <p className="centered-txt">Quality Ingredients</p>
                         <img src={qualityImg}></img>
                     </div>
                     <div className="grid-item-02">
-                            <img src={pickupImg}></img>
+                        <p className="bottom-txt">In Store Pickup</p>
+                        <img src={pickupImg}></img>
                     </div>
                     <div className="grid-item-03"> 
-                            <img src={deliveryImg}></img>
+                        <img src={truckIcon} className="centerd-icon" id="truckIcon"></img>
+                        <img src={deliveryImg}></img>
                     </div>
                 </div>
             </section>    
