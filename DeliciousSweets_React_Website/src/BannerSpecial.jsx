9f781a38d647d4735795d11193574c8be27e229a
Victoria <piklad1996@gmail.com>
2024-01-25 15:53:55 -0600

    update

---
+++ DeliciousSweets_React_Website/src/BannerSpecial.jsx
@@ -0,0 +1,11 @@
+export default function BannerSpecial(){
+
+    return(
+        <>
+            <section>
+                <h1> - TODO: Carousel Banner of current specials here -</h1>
+            </section>
+        
+        </>
+    )
+}
--- DeliciousSweets_React_Website/src/pages/Home.jsx
+++ DeliciousSweets_React_Website/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import qualityImg from "/src/images/siftingOverCake.png";
 import pickupImg from "/src/images/service_img.png";
 import deliveryImg from "/src/images/delivery_img.png";
 import truckIcon from "/src/images/delivery_truck.png";
+import BannerSpecial from "../BannerSpecial";
 
 export default function Home (){
 
@@ -15,19 +16,30 @@ export default function Home (){
             <section id="info-imgs-container">
                 <div className="info-imgs-grid">
                     <div className="grid-item-01">
-                        <p className="centered-txt">Quality Ingredients</p>
+                        <p className="centered-txt large-font">Quality Ingredients</p>
                         <img src={qualityImg}></img>
                     </div>
                     <div className="grid-item-02">
-                        <p className="bottom-txt">In Store Pickup</p>
+                        <p className="bottom-right-txt  medium-font ">In Store Pickup</p>
                         <img src={pickupImg}></img>
                     </div>
                     <div className="grid-item-03"> 
-                        <img src={truckIcon} className="centerd-icon" id="truckIcon"></img>
+                        <img src={truckIcon} className="centered-icon" id="truckIcon"></img>
+                        <p className="lower-centered-txt large-font">Delivery</p>
+                        <div>
+                            <p className="bottom-centered-txt small-font">* Only avaliable within a 15 mile radius</p>
+                        </div>
+                      
                         <img src={deliveryImg}></img>
                     </div>
                 </div>
             </section>    
+
+            <BannerSpecial/>
+
+            <section>
+                <h1>- TODO: "Our Story" here-</h1>
+            </section>
         </>
     )
 }
