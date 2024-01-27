b8e614405e7fb1b31f1a1a06f4e03b50dcbb7fcb
Victoria <piklad1996@gmail.com>
2024-01-27 12:03:19 -0600

    cmt

--- DeliciousSweets_React_Website/src/BannerSpecial.jsx
+++ DeliciousSweets_React_Website/src/BannerSpecial.jsx
@@ -7,7 +7,7 @@ export default function BannerSpecial(){
         const [currentImage, setCurrentImage] = useState(0);
 
         const nextSlide = () => {
-            /*  Reads: If the prevImageIndex is eactly eqaul last index (index count starts at 0 so - 1), 
+            /*  Reads: If the prevImageIndex is exactly eqaul last index, 
             **      then go to 0 index(1st image), 
             **      else go to next image (+1 on the index)
             */
@@ -25,7 +25,6 @@ export default function BannerSpecial(){
                 <button  className="carousel_btn">Prev</button>
                 <img src='{images[currentImage]}' className='carousel_img'/>
                 <button className="carousel_btn">Next</button>
-                
             </section>
         </>
    
