54890ff7a10ce237fbab2d49d521b0837ec0d57c
Victoria <piklad1996@gmail.com>
2024-02-01 13:51:36 -0600

    update

--- DeliciousSweets_React_Website/src/NavBar.jsx
+++ DeliciousSweets_React_Website/src/NavBar.jsx
@@ -12,18 +12,17 @@ export default function Navbar() {
 
         <nav className="nav-menu">
         <Link to="/" ><img src={logo}></img> </Link>
+        <div className="search-container"> 
+            <i class="bi bi-search search-icon"></i> 
+            <input type="text" id="search"/>
+        </div>
             <ul>
-          
-                <li> 
-                    <label htmlFor="search">Search: </label>
-                    <input type="text" id="search"/>
-                </li>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/contact">Contact Us</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/products">Products</Link></li>
                 <li><Link to="/cart">Cart <i className="bi bi-cart2"></i></Link></li>
-                <Link to="" id="close-menu"><i class="bi bi-list"></i></Link>
+                {/* <Link to="" id="close-menu"><i class="bi bi-list"></i></Link> */}
             </ul>
         </nav>
     </section>
