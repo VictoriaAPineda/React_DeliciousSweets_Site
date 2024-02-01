3a886a5f21eddf0358d1a260d85cb3f0cbcf7745
Victoria <piklad1996@gmail.com>
2024-02-01 15:34:36 -0600

    mobile menu

--- DeliciousSweets_React_Website/src/NavBar.jsx
+++ DeliciousSweets_React_Website/src/NavBar.jsx
@@ -11,20 +11,23 @@ export default function Navbar() {
     <section id="header">
 
         <nav className="nav-menu">
-        <Link to="/" ><img src={logo}></img> </Link>
-        <div className="search-container"> 
-            <i class="bi bi-search search-icon"></i> 
-            <input type="text" id="search"/>
-        </div>
+            <Link to="/" ><img src={logo}></img> </Link>
+            <div className="search-container"> 
+                <i class="bi bi-search search-icon"></i> 
+                <input type="text" id="search"/>
+            </div>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/contact">Contact Us</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/products">Products</Link></li>
                 <li><Link to="/cart">Cart <i className="bi bi-cart2"></i></Link></li>
-                {/* <Link to="" id="close-menu"><i class="bi bi-list"></i></Link> */}
+                <Link to="" id="close-nav-btn"><i class="bi bi-x"></i></Link>
             </ul>
         </nav>
+        <div id="mobile">
+            <Link to="" ><i className="bi bi-list list"></i></Link>
+        </div>
     </section>
     </>
   )
