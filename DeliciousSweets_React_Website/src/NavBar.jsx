129761d49bb071d6baaf5f8c007098dc0f06b16c
Victoria <piklad1996@gmail.com>
2024-02-01 16:40:19 -0600

    update

--- DeliciousSweets_React_Website/src/NavBar.jsx
+++ DeliciousSweets_React_Website/src/NavBar.jsx
@@ -12,31 +12,34 @@ export default function Navbar() {
 
   return (
     <>
-    <div id="top-border">
-        <p>- Keeping it Delicious since 1994 -</p>
+        {/* <div id="header-wrapper"> */}
+            <div id="top-border">
+                <p>- Keeping it Delicious since 1994 -</p>
 
-    </div>
-    <section id="header">
-
-        <nav className="nav-menu">
-            <Link to="/" ><img src={logo}></img> </Link>
-            <div className="search-container"> 
-                <i class="bi bi-search search-icon"></i> 
-                <input type="text" id="search"/>
             </div>
-            {/* if the toggle value is true, add class is-open, otherwise it's hidden */}
-            <ul className= {`${isOpen ? "is-open" : ""}`}>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/contact">Contact Us</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/products">Products</Link></li>
-                <li><Link to="/cart">Cart <i className="bi bi-cart2"></i></Link></li>
-            </ul>
-        </nav>
-        {/* list icon for mobile menu */}
-        <Link to="" ><i className="bi bi-list list-trigger" onClick={toggleMenu}></i></Link>
-    </section>
+            <section id="header">
+
+                <nav className="nav-menu">
+                    <Link to="/" ><img src={logo}></img> </Link>
+                    <div className="search-container"> 
+                        <i class="bi bi-search search-icon"></i> 
+                        <input type="text" id="search"/>
+                    </div>
+                    {/* if the toggle value is true, add class is-open, otherwise it's hidden */}
+                    <ul className= {`${isOpen ? "is-open" : ""}`}>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/contact">Contact Us</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/products">Products</Link></li>
+                        <li><Link to="/cart">Cart <i className="bi bi-cart2"></i></Link></li>
+                    </ul>
+                </nav>
+                {/* list icon for mobile menu */}
+                <Link to="" ><i className="bi bi-list list-trigger" onClick={toggleMenu}></i></Link>
+            </section>
+        {/* </div> */}
     </>
+
   )
 }
 
