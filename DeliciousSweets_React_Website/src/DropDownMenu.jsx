2a631434b1dc774a4a75bdbe32f2fb0e9131bd95
Victoria <piklad1996@gmail.com>
2024-02-12 17:38:49 -0600

    dropdown

--- DeliciousSweets_React_Website/src/DropDownMenu.jsx
+++ DeliciousSweets_React_Website/src/DropDownMenu.jsx
@@ -12,7 +12,18 @@ function DropDownMenu({children, ...props}){
     return(
         /* value is what will be used from the context*/
         <DropDownContext.Provider value={{open, setOpen}}>
-
+            <div className='dropdownChild'>{children}</div>
         </DropDownContext.Provider>
+    )
+};
+
+function DropDownButton({children, ...props}){
+    const [open, setOpen] = React.useContext(DropDownContext);/* retrieve context */
+
+    function toggleOpen(){
+        setOpen(!open);
+    }
+    return(
+        <button onClick={toggleOpen}>{children}</button>
     )
 };
