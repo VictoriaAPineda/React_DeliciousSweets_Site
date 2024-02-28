83f8caf36c2bd92dbe60ffb7c280e6bfdb8ea271
Victoria <piklad1996@gmail.com>
2024-02-28 15:29:25 -0600

    update

--- DeliciousSweets_React_Website/src/pages/About.jsx
+++ DeliciousSweets_React_Website/src/pages/About.jsx
@@ -11,7 +11,14 @@ function About(){
                 <img src={topImg} id='topAboutImg'></img>
             </section>
 
-            <section>
+            <section className='about_text_container'>
+                <div>
+                    <p className='large-font about_us_header'>About Us</p>
+                    <p className='about_text'> <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod illo sunt expedita perspiciatis, commodi aliquam delectus quia rem! Nemo rerum corrupti saepe maiores voluptatibus nostrum tenetur quos officia neque eligendi!</span>
+                   <span>Nobis quibusdam vel, adipisci culpa quisquam nihil tempora ipsam veniam, explicabo sapiente, enim iste. Est facilis hic, sed aspernatur blanditiis praesentium nostrum earum, doloremque id facere velit officia odio alias!</span>
+                    <span>Animi, quam molestias perspiciatis cumque mollitia atque optio excepturi fugiat, velit libero voluptate nesciunt doloribus, repudiandae placeat aliquid repellendus iusto aspernatur? Ducimus totam quam dolore doloremque repudiandae officiis dolorem enim.</span></p>
+                </div>
+       
                 <img src={bgtxtImg} id='aboutTextBg'></img>
             </section>
 
