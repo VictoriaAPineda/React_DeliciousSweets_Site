df59775c137b54927ed43a769bb88d302dce9d7f
Victoria <piklad1996@gmail.com>
2024-02-28 13:39:14 -0600

    update

--- DeliciousSweets_React_Website/src/pages/About.jsx
+++ DeliciousSweets_React_Website/src/pages/About.jsx
@@ -1,10 +1,19 @@
 import topImg from '/src/images/about_cake_img.jpg';
+import bgtxtImg from '/src/images/dough_bg.jpg';
 
 function About(){
     return(
         <>
             <section>
                 <img src={topImg} id='topAboutImg'></img>
+            </section>
+
+            <section>
+                <img src={bgtxtImg} id='aboutTextBg'></img>
+            </section>
+
+            <section>
+                
             </section>
         </>
     )
