b1dfd5d7c26bb984ec03ddc3ae5fa1bc3e3c1656
Victoria <piklad1996@gmail.com>
2024-02-28 13:54:25 -0600

    update

--- DeliciousSweets_React_Website/src/pages/About.jsx
+++ DeliciousSweets_React_Website/src/pages/About.jsx
@@ -1,5 +1,8 @@
 import topImg from '/src/images/about_cake_img.jpg';
 import bgtxtImg from '/src/images/dough_bg.jpg';
+import whiteBerryCake01 from '/src/images/whiteCakeWithBerries.jpg';
+import cookies02 from '/src/images/cookieBasket.jpg';
+import cherryCupcakes03 from '/src/images/cherrytopCupcakes.jpg';
 
 function About(){
     return(
@@ -13,7 +16,9 @@ function About(){
             </section>
 
             <section>
-                
+                <img src={whiteBerryCake01}></img>
+                <img src={cookies02}></img>
+                <img src={cherryCupcakes03}></img>
             </section>
         </>
     )
