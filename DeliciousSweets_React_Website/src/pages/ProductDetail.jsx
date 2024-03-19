6b3f789340fffb8d44ee3d605ac4dcc3aad28fbb
Victoria <piklad1996@gmail.com>
2024-03-19 15:52:17 -0500

    notes

--- DeliciousSweets_React_Website/src/pages/ProductDetail.jsx
+++ DeliciousSweets_React_Website/src/pages/ProductDetail.jsx
@@ -4,6 +4,12 @@ export default function ProductDetails(){
     return(
         <>
             Product Details
+
+
+            {/* TODOS: 1.Carousel of items each link(img) to the product's detail page
+            2.Cart be updated in real-time to cart icon on screen via a circle count
+            3. reviews by proudct's id, in own database
+            */}
         </>
     )
 }
