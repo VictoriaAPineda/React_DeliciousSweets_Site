4809b902b7ee8d538417adb3a965fbf30488bd9a
Victoria <piklad1996@gmail.com>
2024-04-08 15:45:04 -0500

    update

--- DeliciousSweets_React_Website/src/MultiCarousel.jsx
+++ DeliciousSweets_React_Website/src/MultiCarousel.jsx
@@ -8,19 +8,19 @@ export default function MultiCarousel(){
         /* TODO: Make adjustments */
         superLargeDesktop: {
           // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
+          breakpoint: { max: 4000, min: 1250 },
           items: 5
         },
         desktop: {
-          breakpoint: { max: 3000, min: 1024 },
+          breakpoint: { max: 1250, min: 900 },
           items: 4
         },
         tablet: {
-          breakpoint: { max: 1024, min: 464 },
+          breakpoint: { max: 900, min: 750 },
           items: 2
         },
         mobile: {
-          breakpoint: { max: 464, min: 0 },
+          breakpoint: { max: 750, min: 0 },
           items: 1
         }
       };
