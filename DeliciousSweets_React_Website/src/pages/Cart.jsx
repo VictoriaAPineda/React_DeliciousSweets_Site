17c645b568c30cf183c56fb76cca302d786cb77a
Victoria <piklad1996@gmail.com>
2024-04-23 16:16:08 -0500

    cart skeleton

--- DeliciousSweets_React_Website/src/pages/Cart.jsx
+++ DeliciousSweets_React_Website/src/pages/Cart.jsx
@@ -1,7 +1,80 @@
+
+
 function Cart(){
     return(
         <>
-        <h1>Cart page :3</h1>
+
+            <section id="cart-page-container">
+                <div className="cart-order-container">
+                    <p>Your Cart of Deliciousness</p>
+                    <div className="order-details-container">
+                        {/*grid 5 cols (last for delete btn)*/}
+
+                    </div>
+
+                    <div className="customer-details-container">
+                        <div className="order-summary-container">
+                            <p>Order Summary</p>
+                            <p>Subtotal</p><div className="subtotal">$12.00</div>
+                            <p>Tax</p><div className="tax">$1.00</div>
+                        </div>
+
+                        <div className="payment-container">
+                            <label htmlFor="">First and Last Name</label>
+                            <input type="text" />
+                            <label htmlFor="">Email Address</label>
+                            <input type="email" />
+                            <label htmlFor="">Phone Number</label>
+                            <input type="text" />
+
+                            <div className="cardPay-container">
+                                <button type="radio"></button>
+                                <p>Credit Card</p>
+                                <button type="radio"></button>
+                                <p>PayPal</p>
+
+                                <p>Card Number</p>
+                                <input type="text"/>
+                                
+                                <p>Expiration Date</p>
+                                <select name="" id="">
+                                    <option value=""></option>
+                                </select>
+                                <select name="" id="">
+                                    <option value=""></option>
+                                </select>
+                                <p>Security Code</p>
+                                <input type="text" maxLength={3} />
+
+
+
+
+                            </div>
+
+                            <div className="order-method-container">
+                                <p>Pick Up Time</p>
+                                <input type="time" name="" id="" min="" max=""/>
+                                <p>OR</p>
+                                <p>Delivery</p>
+                                <label htmlFor="">Address</label>
+                                <input type="text" />
+                                <p>* Note: Will only make deliveries within a 15 mile radius</p>
+                            </div>
+
+                            <button>Place Order</button>
+                            <p>Need Assistance?</p>
+                            <p>555-555-5555</p>
+                        </div>
+                      
+
+                    </div>
+                </div>
+
+            </section>
+
+
+
+       
         </>
     )
 }
