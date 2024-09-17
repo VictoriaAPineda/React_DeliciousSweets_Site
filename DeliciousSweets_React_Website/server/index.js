5d8e020858e166e17940b0350d05a38e5e461eff
Victoria <piklad1996@gmail.com>
2024-09-17 15:11:35 -0500

    router

---
+++ DeliciousSweets_React_Website/server/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+/**
+* Cross Orgin Resource Sharing - allows access to the server
+* from different domains
+**/
+const cors = require('cors')
+const bodyParser = require('body-parser') // for form posting
+const router = require('./routes/router')
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+const corsOptions = {
+    origin:'*',
+    credentials: true,
+    optionSuccessStatus: 200
+}
+app.use({corsOptions})
+app.use('/', router)
+
+
+
+
+const port = 4000 // frontend runs on port 5173
+const server = app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
