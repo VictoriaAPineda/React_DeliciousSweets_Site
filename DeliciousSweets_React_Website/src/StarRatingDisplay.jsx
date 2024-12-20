1235edf523fb21c748cf4155bb15dbb59b75d9a3
Victoria <piklad1996@gmail.com>
2024-12-20 17:56:50 -0600

    component refit

---
+++ DeliciousSweets_React_Website/src/StarRatingDisplay.jsx
@@ -0,0 +1,29 @@
+
+
+export default function StarRatingDisplay({score}){
+    const maxStars = 5;
+  
+    // break down the score inputted
+    const fullStar = Math.floor(score) // holds how many full stars
+    const halfStar = score % 1 !== 0 // holds the half star if any
+    const starArr = [] // holds the resulting star display
+
+    // [1] - Adds on the calculated full stars
+    // A for loop cause there can be multiple full stars
+    for(let i = 0; i < fullStar; i++){
+        starArr.push(<i key={i} className="bi bi-star-fill"></i>)
+    }
+    // [2] - Adds on the calculated half star (if any)
+    // if statement as there can be only 1 half star
+    if(halfStar){
+        starArr.push(<i className="bi bi-star-half"></i>)
+    }
+    // [3] - Construct the star output
+    // each half start counts as "one star"
+    for(let i = fullStar + (halfStar ? 1 : 0); i < maxStars; i++){
+        // Fills up rest of array with empty stars
+        starArr.push(<i class="bi bi-star"></i>)
+    }
+    return starArr
+}
+
--- DeliciousSweets_React_Website/src/Tabs.jsx
+++ DeliciousSweets_React_Website/src/Tabs.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import StarRatingDisplay from "./starRatingDisplay";
 
 function Tabs(){
     const [activeTab, setActiveTab] = useState(0);// tab index 0 default
@@ -16,33 +17,6 @@ function Tabs(){
             { uid: 345, name: "bobRiley345", review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque auctor, erat id vestibulum finibus, ante ante rutrum velit, at tincidunt enim odio nec purus. Vestibulum nisl dolor, tincidunt eleifend mauris id, porta malesuada magna. Proin id lectus sodales, gravida arcu eget, gravida tortor. Etiam laoreet ornare nunc eget gravida. Fusce blandit mattis ipsum sit amet viverra. In nibh justo, iaculis nec odio eu, ornare rhoncus ipsum. Donec id tempus dolor.", rating: 1.5 ,id: "product id: 00"}, 
         ]}
     ];
-
-    {/* Star Rating */}
-    const maxStars = 5;
-    const starRatingDisplay = (score) =>{
-        // break down the score inputted
-        const fullStar = Math.floor(score) // holds how many full stars
-        const halfStar = score % 1 !== 0 // holds the half star if any
-        const starArr = [] // holds the resulting star display
-
-        // [1] - Adds on the calculated full stars
-        // A for loop cause there can be multiple full stars
-        for(let i = 0; i < fullStar; i++){
-            starArr.push(<i key={i} className="bi bi-star-fill"></i>)
-        }
-        // [2] - Adds on the calculated half star (if any)
-        // if statement as there can be only 1 half star
-        if(halfStar){
-            starArr.push(<i className="bi bi-star-half"></i>)
-        }
-        // [3] - Construct the star output
-        // each half start counts as "one star"
-        for(let i = fullStar + (halfStar ? 1 : 0); i < maxStars; i++){
-            // Fills up rest of array with empty stars
-            starArr.push(<i class="bi bi-star"></i>)
-        }
-        return starArr
-    }
 
     return (
         <div>
@@ -73,8 +47,7 @@ function Tabs(){
                         {tabs[1].content.map( review =>(
                         <div className="review-container" key={review.uid}>
                             <p className="username">{review.name}</p>
-                            {/* <p>{review.rating}</p> */}
-                            {starRatingDisplay(review.rating)}
+                            <StarRatingDisplay score = {review.rating}/>
                             <p className="review">"{review.review}"</p>
                         </div>
                         ))}
