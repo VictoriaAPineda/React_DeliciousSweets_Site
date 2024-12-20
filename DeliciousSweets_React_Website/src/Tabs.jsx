a20b16256c55253713f3df7ad0a30537979457be
Victoria <piklad1996@gmail.com>
2024-12-20 16:14:27 -0600

    start rating display

--- DeliciousSweets_React_Website/src/Tabs.jsx
+++ DeliciousSweets_React_Website/src/Tabs.jsx
@@ -9,14 +9,40 @@ function Tabs(){
     Each reveiw will be its own obj in a array of objects from the db 
     data brough in will be based on product id
     */}
-
     const tabs = [
-        { label: 'Specifications', content: 'Specifications: [link up to product id specs]' },
+        { label: 'Specifications', content: 'Specifications:  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget neque imperdiet, mollis ipsum at, iaculis nunc. Curabitur ut interdum ipsum, eget tempor est. Donec lectus turpis, congue quis enim ac, pulvinar semper nulla. Mauris in scelerisque sapien. Pellentesque eget dolor mauris. Duis molestie odio ut interdum rhoncus. Etiam lacinia elementum blandit. Nulla sed ex nisi. Vivamus eu eros sollicitudin, hendrerit lorem ac, elementum felis.' },
         { label: 'Reviews', content :[ 
-            { uid: 123, name: "deb", review: "review 01", rating: 4 ,id: "product id: 00"}, 
-            { uid: 345, name: "bob", review: "review 02", rating: 1 ,id: "product id: 00"}, 
+            { uid: 123, name: "debLefor123", review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque auctor, erat id vestibulum finibus, ante ante rutrum velit, at tincidunt enim odio nec purus. Vestibulum nisl dolor, tincidunt eleifend mauris id, porta malesuada magna. Proin id lectus sodales, gravida arcu eget, gravida tortor. Etiam laoreet ornare nunc eget gravida. Fusce blandit mattis ipsum sit amet viverra. In nibh justo, iaculis nec odio eu, ornare rhoncus ipsum. Donec id tempus dolor.", rating: 4 ,id: "product id: 00"}, 
+            { uid: 345, name: "bobRiley345", review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque auctor, erat id vestibulum finibus, ante ante rutrum velit, at tincidunt enim odio nec purus. Vestibulum nisl dolor, tincidunt eleifend mauris id, porta malesuada magna. Proin id lectus sodales, gravida arcu eget, gravida tortor. Etiam laoreet ornare nunc eget gravida. Fusce blandit mattis ipsum sit amet viverra. In nibh justo, iaculis nec odio eu, ornare rhoncus ipsum. Donec id tempus dolor.", rating: 1.5 ,id: "product id: 00"}, 
         ]}
     ];
+
+    {/* Star Rating */}
+    const maxStars = 5;
+    const starRatingDisplay = (score) =>{
+        // break down the score inputted
+        const fullStar = Math.floor(score) // holds how many full stars
+        const halfStar = score % 1 !== 0 // holds how many half stars
+
+        console.log("full: " + fullStar + " half: " + halfStar)
+        const starArr = [] // holds the resulting star display
+
+        // [1] - Adds on the calculated full stars
+        for(let i = 0; i < fullStar; i++){
+            starArr.push(<i key={i} className="bi bi-star-fill"></i>)
+        }
+        // [2] - Adds on the calculated half stars
+        if(halfStar){
+            starArr.push(<i className="bi bi-star-half"></i>)
+        }
+        // [3] - Construct the star output
+        // each half start counts as "one star"
+        for(let i = fullStar + (halfStar ? 1 : 0); i < maxStars; i++){
+            // Fills up rest of array with empty stars
+            starArr.push(<i class="bi bi-star"></i>)
+        }
+        return starArr
+    }
 
     return (
         <div>
@@ -36,15 +62,20 @@ function Tabs(){
                 {/* Specs*/}
                 { activeTab === 0 && <p> {tabs[activeTab].content}</p> }
                 {/* Reviews*/}
-                {/* Loop through reviews...
-                TODO: Create mock data. Limit to 5 reviews per page? Mini pagnation
-                Order by highest to least (rating number)*/}
+                {/* 
+                    TODOS: Create mock data. Limit to 5 reviews per page? 
+                    Mini pagnation
+                    Order by highest to least (rating number)
+                    ratings be displayed out of 5 stars
+                */}
                  { activeTab === 1 && 
                     <div>
-                        {tabs[1].content.map( i =>(
-                        <div key={i.uid}>
-                            <h2>{i.uid}</h2>
-                            <h1>{i.name}</h1>
+                        {tabs[1].content.map( review =>(
+                        <div className="review-container" key={review.uid}>
+                            <p className="username">{review.name}</p>
+                            {/* <p>{review.rating}</p> */}
+                            {starRatingDisplay(review.rating)}
+                            <p className="review">"{review.review}"</p>
                         </div>
                         ))}
                     </div>       
